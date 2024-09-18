d2310ba837f88a0b067765c2f461bea8f9331cc7
Jonathan Briganti <51672261+datajonbrig@users.noreply.github.com>
2024-09-18 14:12:22 -0400

    cleaned up files, added setup scripts
    cleaned up Jupyter notebook for more simple use in binder, added a setup and post build script to auto-install gnina for users

---
+++ postbuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the setup script to download and prepare gnina
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download gnina
+wget https://github.com/gnina/gnina/releases/download/v1.0.3/gnina
+
+# Make gnina executable
+chmod +x gnina
